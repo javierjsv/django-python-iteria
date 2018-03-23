90910c385185dd116a989e4bc1e9fd840e313aea
javierjsv <javierjsv@hotmail.com>
2018-03-23 17:35:59 -0500

    projecto con su entorno

---
+++ Iteria/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Iteria project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Iteria.settings")
+
+application = get_wsgi_application()
---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Usuario1\Documents\javier solis v\projectos nuevos\django\iteria\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
